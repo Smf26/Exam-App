b445953d5aba4ac2cc8b47b242c9f2c4e8bed687
Shepherd Magwagwa <tinshepherds@yahoo.com>
2020-06-19 00:07:49 +0200

    build created

---
+++ js/science.js
@@ -0,0 +1,157 @@
+const Science = [
+  {
+    question: 'What species is the closest living relative to humans ?.',
+    answers : [
+      { text: 'Chimpanzees', correct: true },
+      { text: 'Gorilla', correct: false },
+      { text: 'Monkey', correct: false },
+      { text: 'Hyena', correct: false}
+    ]
+  },
+  {
+    question: 'What\'s the base unit of mass in the metric system ?.',
+    answers : [
+      { text: 'Kilowatt', correct: false },
+      { text: 'Kilometer', correct: false },
+      { text: 'Kiloliter', correct: false },
+      { text: 'Kilogram', correct: true }
+    ]
+  },
+  {
+    question: 'In what galaxy is the solar system located ?.',
+    answers : [
+      { text: 'Saturn', correct: false },
+      { text: 'Pluto', correct: false },
+      { text: 'Milky Way', correct: true },
+      { text: 'Mars', correct: false}
+    ]
+  },
+  {
+    question: 'What O-word describes oxygen with molecules that have three atoms instead of two ?.',
+    answers : [
+      { text: 'Omitte', correct: false },
+      { text: 'Ozone', correct: true },
+      { text: 'Oburne', correct: false },
+      { text: 'Odine', correct: false }
+    ]
+  },
+  {
+    question: 'How many of the nine planets have moons ?.',
+    answers : [
+      { text: 'Nine', correct: false },
+      { text: 'Seven', correct: true },
+      { text: 'One', correct: false },
+      { text: 'Five', correct: false }
+    ]
+  },
+  {
+    question: 'What cooking fuel is produced by heating wood without oxygen ?.',
+    answers : [
+      { text: 'Gas', correct: false },
+      { text: 'Charcoal', correct: true },
+      { text: 'Wood', correct: false },
+      { text: 'Thermite', correct: false }
+    ]
+  },
+  {
+    question: 'What type of trees yield the resin used to produce turpentine ?.',
+    answers : [
+      { text: 'WhiteOak tree', correct: false },
+      { text: 'Gum tree ', correct: false },
+      { text: 'Mango tree', correct: false },
+      { text: 'Pine tree', correct: true }
+    ]
+  },
+  {
+    question: 'What\'s the most malleable metal ?.',
+    answers : [
+      { text: 'Copper', correct: false },
+      { text: 'Aluminum ', correct: false },
+      { text: 'Brass', correct: false },
+      { text: 'Gold', correct: true }
+    ]
+  },
+  {
+    question: 'What continent is subjected to the world\'s largest ozone hole ?.',
+    answers : [
+      { text: 'Antarctica', correct: true },
+      { text: 'Iceland', correct: false },
+      { text: 'Africa', correct: false },
+      { text: 'Europe', correct: false }
+    ]
+  },
+  {
+    question: 'What is the chemical formula for water ?.',
+    answers : [
+      { text: 'O2Q', correct: false },
+      { text: 'S2O', correct: false },
+      { text: 'H2Q', correct: false },
+      { text: 'H2O', correct: true }
+    ]
+  },
+  {
+    question: 'What process involves heating an ore to obtain a metal ?.',
+    answers : [
+      { text: 'Brazing', correct: false },
+      { text: 'Graining', correct: false },
+      { text: 'Smelting', correct: true },
+      { text: 'Mulping', correct: false }
+    ]
+  },
+  {
+    question: 'What\'s removed from water in the process of desalination ?.',
+    answers : [
+      { text: 'Salt', correct: true },
+      { text: 'Sugar', correct: false },
+      { text: 'Germs', correct: false },
+      { text: 'Mineral', correct: false }
+    ]
+  },
+  {
+    question: 'What planet is closest in size to our moon ?.',
+    answers : [
+      { text: 'Mars', correct: false },
+      { text: 'Jupiter', correct: false },
+      { text: 'Mercury', correct: true },
+      { text: 'Pluto', correct: false }
+    ]
+  },
+  {
+    question: 'What type of mineral is pencil "lead" made from ?.',
+    answers : [
+      { text: 'Coal', correct: false },
+      { text: 'Graphite', correct: true },
+      { text: 'Iron ore', correct: false },
+      { text: 'Sulphate', correct: false }
+    ]
+  },
+  {
+    question: 'What color does litmus turn when dipped into acid ?.',
+    answers : [
+      { text: 'Red', correct: false },
+      { text: 'Pink', correct: true },
+      { text: 'Blue', correct: false },
+      { text: 'Black', correct: false }
+    ]
+  },
+  {
+    question: 'What\'s the only metal that\'s not a solid at room temperature ?.',
+    answers : [
+      { text: 'Mercury', correct: true },
+      { text: 'Valarian Steel', correct: false },
+      { text: 'Iron ore', correct: false },
+      { text: 'Gold', correct: false }
+    ]
+  },
+  {
+    question: 'What\'s the common name for a cubic decimeter ?.',
+    answers : [
+      { text: 'A cubic', correct: false },
+      { text: 'A liter', correct: true },
+      { text: 'A milimeter', correct: false },
+      { text: 'A gram', correct: false }
+    ]
+  }
+]
+
+export default Science;
